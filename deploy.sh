7c9caf0eaca47f5644da8bad3f1344258fd269c9
Elton Minetto <eminetto@gmail.com>
2018-04-09 19:11:44 -0300

    Atualiza contato e corrige problema de deploy

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ BUCKET_NAME=eltonminetto.net
 hugo
 
 # Copy over pages - not static js/img/css/downloads
-aws s3 sync --acl "public-read" public/ s3://$BUCKET_NAME/ --exclude 'img' --exclude 'js' --exclude 'css' --exclude 'post'
+aws --profile eltonminetto.net s3 sync --acl "public-read" public/ s3://$BUCKET_NAME/ --exclude 'img' --exclude 'js' --exclude 'css' --exclude 'post'
 
 # Ensure static files are set to cache forever - cache for a month --cache-control "max-age=2592000"
-aws s3 sync --cache-control "max-age=2592000" --acl "public-read" public/img/ s3://$BUCKET_NAME/img/
-aws s3 sync --cache-control "max-age=2592000" --acl "public-read" public/css/ s3://$BUCKET_NAME/css/
-aws s3 sync --cache-control "max-age=2592000" --acl "public-read" public/js/ s3://$BUCKET_NAME/js/
+aws --profile eltonminetto.net s3 sync --cache-control "max-age=2592000" --acl "public-read" public/img/ s3://$BUCKET_NAME/img/
+aws --profile eltonminetto.net s3 sync --cache-control "max-age=2592000" --acl "public-read" public/css/ s3://$BUCKET_NAME/css/
+aws --profile eltonminetto.net s3 sync --cache-control "max-age=2592000" --acl "public-read" public/js/ s3://$BUCKET_NAME/js/
